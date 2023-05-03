62c92b5f79e01b23583c863b776b4feda29fcd8d
iqolll <muhammadhaiqal1802@gmai.com>
2023-05-04 05:38:47 +0700

    add pagination for search

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: 'd81cb34b023e9cc5fd3222f07bcf098f',
@@ -256,7 +257,11 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if (global.search.term != '' && global.search.term != null) {
-    const { results } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
 
     if (results.length === 0) {
       showAlert('No results found');
@@ -272,6 +277,11 @@ async function search() {
 }
 
 function displaySearchResult(results) {
+  // Clear previous results
+  document.querySelector('#search-results').innerHTML = ``;
+  document.querySelector('#search-results-heading').innerHTML = ``;
+  document.querySelector('#pagination').innerHTML = ``;
+
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -308,7 +318,51 @@ function displaySearchResult(results) {
               </p>
             </div>
           `;
+
+    document.querySelector('#search-results-heading').innerHTML = `
+    <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+    `;
+
     document.querySelector('#search-results').appendChild(div);
+  });
+
+  displayPagination();
+}
+
+// Create & Display Pagination For Search
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  // Disable Prev Button if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  // Disable next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResult(results);
+  });
+
+  // Previous page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResult(results);
   });
 }
 
@@ -387,7 +441,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
